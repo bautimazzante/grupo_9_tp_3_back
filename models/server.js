c3d24c05d04d36df4646ecbaac5b11e027c556cc
THITITAPUERCA <Thiagojtapuerca@gmail.com>
2026-06-08 18:09:28 -0300

    solucion de errores en los servicios

--- models/server.js
+++ models/server.js
@@ -15,6 +15,7 @@ class Server {
   middlewares() {
     this.app.use(cors());
     this.app.use(express.json());
+    this.app.use(express.static(path.join(__dirname, "../assets")));
   }
 
   routes() {
